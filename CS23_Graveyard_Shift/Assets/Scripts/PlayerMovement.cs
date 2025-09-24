89532496ecff62c52ae7ab2496a5a99e94339f92
hanksun2173 <hanru.sun@tufts.edu>
2025-09-24 15:36:08 -0400

    Player fence collisions

--- CS23_Graveyard_Shift/Assets/Scripts/PlayerMovement.cs
+++ CS23_Graveyard_Shift/Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-     public Rigidbody2D rb;
-     public float moveSpeed = 5f;
-     public Vector2 movement;
-    //  public GameHandler gameHandlerObj;
+    public Rigidbody2D rb;
+    public float moveSpeed = 10f;
+    public float collisionOffset = 0.05f;
+    public Vector2 movement;
+    public ContactFilter2D movementFilter;
+    public RaycastHit2D[] castCollisions = new RaycastHit2D[10];
 
-     void Start(){
-          rb = GetComponent<Rigidbody2D> ();
-        //   if (GameObject.FindWithTag("GameHandler") != null){
-        //        gameHandlerObj = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
-        //   }
-     }
+    void Start() {
+        rb = GetComponent<Rigidbody2D> ();
+    }
 
-     void FixedUpdate(){
-          movement.x = Input.GetAxisRaw ("Horizontal");
-          movement.y = Input.GetAxisRaw ("Vertical");
-          rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-     }
+    public bool MovePlayer() {
+
+        movement.x = Input.GetAxisRaw ("Horizontal");
+        movement.y = Input.GetAxisRaw ("Vertical");
+
+        if (movement != Vector2.zero) {
+            int count = rb.Cast(
+                movement.normalized,
+                movementFilter,
+                castCollisions,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+            if (count == 0){
+                rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    void FixedUpdate() {
+        bool moved = MovePlayer();
+    }
 
 }
