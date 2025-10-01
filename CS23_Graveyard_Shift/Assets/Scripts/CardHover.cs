6b6f6f16f843e4f2660cb610f90d6d1974bd9348
Oliver <ozhang2305@gmail.com>
2025-09-30 20:29:48 -0400

    update card display

--- CS23_Graveyard_Shift/Assets/Scripts/CardHover.cs
+++ CS23_Graveyard_Shift/Assets/Scripts/CardHover.cs
@@ -9,12 +9,18 @@ public class CardHover : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Vector3 initialScale;
+    private int cardValue;
+
     public GameObject EnemyHealth;
+    public GameObject CardValueDisplay;
+    public GameObject CardSuitDisplay;
+
     void Start()
     {
         initialScale = transform.localScale;
         EnemyHealth.SetActive(true);
-        
+        CardValueDisplay.SetActive(true);
+        CardSuitDisplay.SetActive(true);
     }
 
     // Update is called once per frame
@@ -26,11 +32,15 @@ public class CardHover : MonoBehaviour
     void OnMouseEnter()
     {
         transform.localScale = initialScale * 1.1f;
+
+        CardValueDisplay.GetComponent<Text>().text = cardValue.ToString();
     }
 
     void OnMouseExit()
     {
         transform.localScale = initialScale;
+
+        CardValueDisplay.GetComponent<Text>().text = "";
     }
 
     void OnMouseDown()
