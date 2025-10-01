1f4dbbe2527bf167dadfd8f0dc23144ef06af960
hanksun2173 <hanru.sun@tufts.edu>
2025-09-30 21:00:23 -0400

    A lot of changes

--- CS23_Graveyard_Shift/Assets/Scripts/CardGenerator.cs
+++ CS23_Graveyard_Shift/Assets/Scripts/CardGenerator.cs
@@ -1,37 +1,37 @@
-using System.Collections.Generic;
-using System.Collections;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+// using System.Collections.Generic;
+// using System.Collections;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using UnityEngine.SceneManagement;
 
-public class CardGenerator : MonoBehaviour {
+// public class CardGenerator : MonoBehaviour {
 
-    // public GameObject gravePrefab;
-    // public Vector2 spawnPosition = new Vector2(30, 10);
-    // public Vector2[] SpawnPoints = new Vector2[numberOfGraves];
+//     // public GameObject gravePrefab;
+//     // public Vector2 spawnPosition = new Vector2(30, 10);
+//     // public Vector2[] SpawnPoints = new Vector2[numberOfGraves];
 
-    public GameObject cardPrefab;
-    public int handSize = 5;
+//     public GameObject cardPrefab;
+//     public int handSize = 5;
 
-    void Start() {
-        for (int i = 0; i < numberOfGraves; i++) {
+//     void Start() {
+//         for (int i = 0; i < numberOfGraves; i++) {
 
-            int cardVal = Random.Next(0, 11);
-            int cardSuit = Random.Next(0, 3);
+//             int cardVal = Random.Next(0, 11);
+//             int cardSuit = Random.Next(0, 3);
 
             
 
-            for (int j = 0; j < i; j++) {
-                if (Vector2.Distance(spawnPos, SpawnPoints[j]) < 10.0f) {
-                    spawnPos = new Vector2(
-                        Random.Range(-spawnPosition.x, spawnPosition.x),
-                        Random.Range(-spawnPosition.y, spawnPosition.y)
-                    );
-                    j = -1;
-                }
-            }
-            Instantiate(gravePrefab, spawnPos, Quaternion.identity);
-            SpawnPoints[i] = spawnPos;
-        }
-    }
-}
+//             for (int j = 0; j < i; j++) {
+//                 if (Vector2.Distance(spawnPos, SpawnPoints[j]) < 10.0f) {
+//                     spawnPos = new Vector2(
+//                         Random.Range(-spawnPosition.x, spawnPosition.x),
+//                         Random.Range(-spawnPosition.y, spawnPosition.y)
+//                     );
+//                     j = -1;
+//                 }
+//             }
+//             Instantiate(gravePrefab, spawnPos, Quaternion.identity);
+//             SpawnPoints[i] = spawnPos;
+//         }
+//     }
+// }
