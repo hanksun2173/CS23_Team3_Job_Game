6e608a1e9b1051abe56ffd5bb18802effe6df390
MaveyMavV <mavenli2468@gmail.com>
2025-10-14 18:12:05 -0400

    ENDSCENEN

--- CS23_Graveyard_Shift/Assets/Scripts/RestartOnWin.cs
+++ CS23_Graveyard_Shift/Assets/Scripts/RestartOnWin.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class RestartOnWin : MonoBehaviour
 {
     public void RestartGame()
     {
         Time.timeScale = 1f;
         GameHandler_PauseMenu.GameisPaused = false;
-        playerHealth = 5;
         SceneManager.LoadScene("Menu");
         
     }
